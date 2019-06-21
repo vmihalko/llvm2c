84c53b2a8e0323627f0e949483ad7f12c0e7c9c7
Tomas Jasek <tomsik68@gmail.com>
2019-06-21 13:55:42 +0200

    Add SimplifyingExprVisitor
    This will help with simplifying all expressions within AST

---
+++ parser/SimplifyingExprVisitor.cpp
@@ -0,0 +1,197 @@
+#include "SimplifyingExprVisitor.h"
+
+void SimplifyingExprVisitor::visit(StructElement& expr) {
+    expr.expr->accept(*this);
+    expr.expr = simplify(expr.expr);
+}
+
+void SimplifyingExprVisitor::visit(ArrayElement& ae) {
+    ae.expr->accept(*this);
+    ae.element->accept(*this);
+
+    ae.expr = simplify(ae.expr);
+    ae.element = simplify(ae.element);
+}
+
+void SimplifyingExprVisitor::visit(ExtractValueExpr& expr) {
+    for (auto& index : expr.indices) {
+        index->accept(*this);
+    }
+}
+
+void SimplifyingExprVisitor::visit(IfExpr& ifExpr) {
+    if (ifExpr.cmp) {
+        ifExpr.cmp->accept(*this);
+        ifExpr.cmp = simplify(ifExpr.cmp);
+    }
+}
+
+void SimplifyingExprVisitor::visit(SwitchExpr& expr) {
+    expr.cmp->accept(*this);
+    expr.cmp = simplify(expr.cmp);
+}
+
+void SimplifyingExprVisitor::visit(AsmExpr& expr) {
+}
+
+void SimplifyingExprVisitor::visit(CallExpr& expr) {
+    if (expr.funcValue) {
+        expr.funcValue->accept(*this);
+        expr.funcValue = simplify(expr.funcValue);
+    }
+
+    for (auto it = expr.params.begin(); it != expr.params.end(); ++it) {
+        (*it)->accept(*this);
+        *it = simplify(*it);
+    }
+}
+
+void SimplifyingExprVisitor::visit(PointerShift& expr) {
+    expr.pointer->accept(*this);
+    expr.move->accept(*this);
+
+    expr.pointer = simplify(expr.pointer);
+    expr.move = simplify(expr.move);
+}
+
+void SimplifyingExprVisitor::visit(GepExpr& expr) {
+    for (auto& index : expr.indices) {
+        index->accept(*this);
+    }
+}
+
+void SimplifyingExprVisitor::visit(SelectExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+    expr.comp->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+    expr.comp = simplify(expr.comp);
+}
+
+void SimplifyingExprVisitor::visit(RefExpr& expr) {
+    expr.expr->accept(*this);
+    expr.expr = simplify(expr.expr);
+}
+
+void SimplifyingExprVisitor::visit(DerefExpr& expr) {
+    expr.expr->accept(*this);
+    expr.expr = simplify(expr.expr);
+}
+
+void SimplifyingExprVisitor::visit(RetExpr& expr) {
+    if (expr.expr) {
+        expr.expr->accept(*this);
+        expr.expr = simplify(expr.expr);
+    }
+}
+
+void SimplifyingExprVisitor::visit(CastExpr& expr) {
+    expr.expr->accept(*this);
+    expr.expr = simplify(expr.expr);
+}
+
+void SimplifyingExprVisitor::visit(AddExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(SubExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(AssignExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(MulExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(DivExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(RemExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(AndExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(OrExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(XorExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(CmpExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(AshrExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(LshrExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
+
+void SimplifyingExprVisitor::visit(ShlExpr& expr) {
+    expr.left->accept(*this);
+    expr.right->accept(*this);
+
+    expr.left = simplify(expr.left);
+    expr.right = simplify(expr.right);
+}
--- parser/ref-deref.cpp
+++ parser/ref-deref.cpp
@@ -4,40 +4,12 @@
 #include "../core/Func.h"
 #include "../core/Block.h"
 
-#include "../expr/ExprVisitor.h"
+#include "SimplifyingExprVisitor.h"
 
-class RefDerefVisitor : public ExprVisitor {
+class RefDerefVisitor : public SimplifyingExprVisitor {
 
-    Expr* simplify(Expr* expr);
-public:
-    void visit(StructElement& expr) override;
-    void visit(ArrayElement& expr) override;
-    void visit(ExtractValueExpr& expr) override;
-    void visit(IfExpr& expr) override;
-    void visit(SwitchExpr& expr) override;
-    void visit(AsmExpr& expr) override;
-    void visit(CallExpr& expr) override;
-    void visit(PointerShift& expr) override;
-    void visit(GepExpr& expr) override;
-    void visit(SelectExpr& expr) override;
-    void visit(RefExpr& expr) override;
-    void visit(DerefExpr& expr) override;
-    void visit(RetExpr& expr) override;
-    void visit(CastExpr& expr) override;
-    void visit(AddExpr& expr) override;
-    void visit(SubExpr& expr) override;
-    void visit(AssignExpr& expr) override;
-    void visit(MulExpr& expr) override;
-    void visit(DivExpr& expr) override;
-    void visit(RemExpr& expr) override;
-    void visit(AndExpr& expr) override;
-    void visit(OrExpr& expr) override;
-    void visit(XorExpr& expr) override;
-    void visit(CmpExpr& expr) override;
-    void visit(AshrExpr& expr) override;
-    void visit(LshrExpr& expr) override;
-    void visit(ShlExpr& expr) override;
-
+protected:
+    Expr* simplify(Expr* expr) override;
 };
 
 void refDeref(const llvm::Module* module, Program& program) {
@@ -58,213 +30,17 @@ void refDeref(const llvm::Module* module, Program& program) {
 }
 
 Expr* RefDerefVisitor::simplify(Expr* expr) {
-    if (auto RE = llvm::dyn_cast<RefExpr>(expr)) {
-        if (auto DE = llvm::dyn_cast<DerefExpr>(RE->expr)) {
+    if (auto RE = llvm::dyn_cast_or_null<RefExpr>(expr)) {
+        if (auto DE = llvm::dyn_cast_or_null<DerefExpr>(RE->expr)) {
             return DE->expr;
         }
     }
 
-    if (auto DE = llvm::dyn_cast<DerefExpr>(expr)) {
-        if (auto RE = llvm::dyn_cast<RefExpr>(DE->expr)) {
+    if (auto DE = llvm::dyn_cast_or_null<DerefExpr>(expr)) {
+        if (auto RE = llvm::dyn_cast_or_null<RefExpr>(DE->expr)) {
             return RE->expr;
         }
     }
 
     return expr;
-}
-
-void RefDerefVisitor::visit(StructElement& expr) {
-    expr.expr->accept(*this);
-    expr.expr = simplify(expr.expr);
-}
-
-void RefDerefVisitor::visit(ArrayElement& ae) {
-    ae.expr->accept(*this);
-    ae.element->accept(*this);
-
-    ae.expr = simplify(ae.expr);
-    ae.element = simplify(ae.element);
-}
-
-void RefDerefVisitor::visit(ExtractValueExpr& expr) {
-    for (auto& index : expr.indices) {
-        index->accept(*this);
-    }
-}
-
-void RefDerefVisitor::visit(IfExpr& ifExpr) {
-    if (ifExpr.cmp) {
-        ifExpr.cmp->accept(*this);
-        ifExpr.cmp = simplify(ifExpr.cmp);
-    }
-}
-
-void RefDerefVisitor::visit(SwitchExpr& expr) {
-    expr.cmp->accept(*this);
-    expr.cmp = simplify(expr.cmp);
-}
-
-void RefDerefVisitor::visit(AsmExpr& expr) {
-}
-
-void RefDerefVisitor::visit(CallExpr& expr) {
-    if (expr.funcValue) {
-        expr.funcValue->accept(*this);
-        expr.funcValue = simplify(expr.funcValue);
-    }
-
-    for (auto it = expr.params.begin(); it != expr.params.end(); ++it) {
-        (*it)->accept(*this);
-        *it = simplify(*it);
-    }
-}
-
-void RefDerefVisitor::visit(PointerShift& expr) {
-    expr.pointer->accept(*this);
-    expr.move->accept(*this);
-
-    expr.pointer = simplify(expr.pointer);
-    expr.move = simplify(expr.move);
-}
-
-void RefDerefVisitor::visit(GepExpr& expr) {
-    for (auto& index : expr.indices) {
-        index->accept(*this);
-    }
-}
-
-void RefDerefVisitor::visit(SelectExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-    expr.comp->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-    expr.comp = simplify(expr.comp);
-}
-
-void RefDerefVisitor::visit(RefExpr& expr) {
-    expr.expr->accept(*this);
-    expr.expr = simplify(expr.expr);
-}
-
-void RefDerefVisitor::visit(DerefExpr& expr) {
-    expr.expr->accept(*this);
-    expr.expr = simplify(expr.expr);
-}
-
-void RefDerefVisitor::visit(RetExpr& expr) {
-    if (expr.expr) {
-        expr.expr->accept(*this);
-        expr.expr = simplify(expr.expr);
-    }
-}
-
-void RefDerefVisitor::visit(CastExpr& expr) {
-    expr.expr->accept(*this);
-    expr.expr = simplify(expr.expr);
-}
-
-void RefDerefVisitor::visit(AddExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(SubExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(AssignExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(MulExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(DivExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(RemExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(AndExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(OrExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(XorExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(CmpExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(AshrExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(LshrExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
-}
-
-void RefDerefVisitor::visit(ShlExpr& expr) {
-    expr.left->accept(*this);
-    expr.right->accept(*this);
-
-    expr.left = simplify(expr.left);
-    expr.right = simplify(expr.right);
 }
