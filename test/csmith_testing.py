da9fcb35c1ea692992a865e7d031652bda26fb8a
Tomas Jasek <tomsik68@gmail.com>
2019-07-01 16:14:37 +0200

    check produced C programs with clang

--- test/csmith_testing.py
+++ test/csmith_testing.py
@@ -6,12 +6,17 @@ csmith_runtime_dir = 'csmith-runtime'
 import tempfile, os
 from subprocess import run, DEVNULL
 
-# with tempfile.TemporaryDirectory() as dirname:
 dirname = 'csmith-tests'
 for i in range(num_tests):
     run(['./csmith', '--output', dirname + '/program.c'])
     run(['clang', '-I', csmith_runtime_dir, '-S', '-emit-llvm', dirname + '/program.c'])
-    proc = run(['./llvm2c', '-p', dirname + '/program.ll'], stdout=DEVNULL)
+    proc = run(['./llvm2c', '-o', dirname + '/output.c', dirname + '/program.ll'])
+
+    if proc.returncode != 0:
+        print("Found bug!")
+        break
+
+    proc = run(['clang', '-I', csmith_runtime_dir, dirname + '/output.c'])
     if proc.returncode != 0:
         print("Found bug!")
         break
