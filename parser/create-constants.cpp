2db7dafa61d49a7b56ecbd76eb1c36514fab0499
Tomas Jasek <tomsik68@gmail.com>
2019-07-23 11:17:30 +0200

    create-constants: handle recursive uses of values

--- parser/create-constants.cpp
+++ parser/create-constants.cpp
@@ -3,20 +3,34 @@
 #include "../core/Program.h"
 #include "constval.h"
 
-static void createConstantsForOperands(const llvm::Instruction& ins, Program& program) {
-    for (const auto& op : ins.operands()) {
+static void createConstantsForOperands(const llvm::User* ins, Program& program) {
+    for (const auto& op : ins->operands()) {
+        if (auto *user = llvm::dyn_cast_or_null<llvm::User>(op.get())) {
+            createConstantsForOperands(user, program);
+        }
+
         if (auto C = llvm::dyn_cast_or_null<llvm::Constant>(op.get())) {
             if (program.getExpr(op.get()) == nullptr)
                 program.addExpr(op.get(), createConstantValue(C, program));
         }
+
     }
 }
 
 void createConstants(const llvm::Module* mod, Program& program) {
+    for (const auto& gvar : mod->globals()) {
+        if (gvar.hasInitializer()) {
+            auto* init = gvar.getInitializer();
+            if (auto *user = llvm::dyn_cast_or_null<llvm::User>(init)) {
+                createConstantsForOperands(user, program);
+            }
+        }
+    }
+
     for (const auto& func : mod->functions()) {
         for (const auto& bb : func) {
             for (const auto& ins : bb) {
-                createConstantsForOperands(ins, program);
+                createConstantsForOperands(&ins, program);
             }
         }
     }
