4e1fd211fe6cd22cdf78a5c8a42fa08a64d1a4d9
Tomas Jasek <tomsik68@gmail.com>
2019-07-25 12:31:18 +0200

    tests: fix order of function defs in math tests

--- test/expected/math/fact_rec.c
+++ test/expected/math/fact_rec.c
@@ -16,6 +16,22 @@ unsigned long fact(unsigned long var0);
 int main(int var0, char** var1);
 unsigned long strtol(unsigned char* var0, unsigned char** var1, unsigned int var2);
 
+unsigned long fact(unsigned long var0){
+    unsigned long var1;
+    unsigned long var2;
+    unsigned long var3;
+    block0: ;
+    var2 = var0;
+    if (((long)var2) > ((long)0)) {
+        var3 = var2;
+        var1 = (var3 * fact(((long)var2) - ((long)1)));
+        return var1;
+    } else {
+        var1 = 1;
+        return var1;
+    }
+}
+
 int main(int var0, char** var1){
     unsigned int var2;
     unsigned int var3;
@@ -38,22 +54,6 @@ int main(int var0, char** var1){
             var2 = ((unsigned int)fact(var6));
             return var2;
         }
-    }
-}
-
-unsigned long fact(unsigned long var0){
-    unsigned long var1;
-    unsigned long var2;
-    unsigned long var3;
-    block0: ;
-    var2 = var0;
-    if (((long)var2) > ((long)0)) {
-        var3 = var2;
-        var1 = (var3 * fact(((long)var2) - ((long)1)));
-        return var1;
-    } else {
-        var1 = 1;
-        return var1;
     }
 }
 
--- test/expected/math/isPrime.c
+++ test/expected/math/isPrime.c
@@ -16,31 +16,6 @@ unsigned int isPrime(unsigned long var0);
 int main(int var0, char** var1);
 unsigned long strtol(unsigned char* var0, unsigned char** var1, unsigned int var2);
 
-int main(int var0, char** var1){
-    unsigned int var2;
-    unsigned int var3;
-    unsigned char** var4;
-    unsigned char* var5;
-    unsigned long var6;
-    block0: ;
-    var2 = 0;
-    var3 = var0;
-    var4 = var1;
-    if (var3 != 2) {
-        var2 = -1;
-        return var2;
-    } else {
-        var6 = strtol(*(((unsigned char**)(var4)) + 1), &var5, 10);
-        if (((unsigned int)(*var5)) != 0) {
-            var2 = -1;
-            return var2;
-        } else {
-            var2 = isPrime(var6);
-            return var2;
-        }
-    }
-}
-
 unsigned int isPrime(unsigned long var0){
     unsigned int var1;
     unsigned long var2;
@@ -66,6 +41,31 @@ goto block3;
     } else {
         var1 = 1;
         return var1;
+    }
+}
+
+int main(int var0, char** var1){
+    unsigned int var2;
+    unsigned int var3;
+    unsigned char** var4;
+    unsigned char* var5;
+    unsigned long var6;
+    block0: ;
+    var2 = 0;
+    var3 = var0;
+    var4 = var1;
+    if (var3 != 2) {
+        var2 = -1;
+        return var2;
+    } else {
+        var6 = strtol(*(((unsigned char**)(var4)) + 1), &var5, 10);
+        if (((unsigned int)(*var5)) != 0) {
+            var2 = -1;
+            return var2;
+        } else {
+            var2 = isPrime(var6);
+            return var2;
+        }
     }
 }
 
