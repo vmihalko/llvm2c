c16b7b03ebfea6ab1aa5ca0846de6dfa476eec0d
Tomas Jasek <tomsik68@gmail.com>
2019-07-26 14:40:48 +0200

    inlineBlocks: use a recursive visitor for inlining

--- parser/inlineBlocks.cpp
+++ parser/inlineBlocks.cpp
@@ -1,29 +1,54 @@
 #include "../core/Program.h"
+#include "SimplifyingExprVisitor.h"
 
 #include <llvm/IR/Instructions.h>
 
 #include <algorithm>
 
+class InliningVisitor : public SimplifyingExprVisitor {
+private:
+    Block* container;
+public:
+    InliningVisitor(Block* container) : container(container) {}
+    Expr* simplify(Expr* expr) override;
+};
+
+Expr* InliningVisitor::simplify(Expr* expr) {
+    if (auto* gotoExpr = llvm::dyn_cast_or_null<GotoExpr>(expr)) {
+        Block* target = gotoExpr->target;
+
+        if (target->doInline) {
+            std::vector<Expr*> targetBlockExpressions;
+            for (auto* expr : target->expressions) {
+                expr->accept(*this);
+                targetBlockExpressions.push_back(simplify(expr));
+            }
+
+            auto list = std::make_unique<ExprList>(std::move(targetBlockExpressions));
+            Expr* result = list.get();
+            container->addOwnership(std::move(list));
+            return result;
+        }
+    }
+
+    return expr;
+}
+
 void inlineBlocks(const llvm::Module* module, Program& program) {
     assert(program.isPassCompleted(PassType::IdentifyInlinableBlocks));
+
 
     for (const llvm::Function& func : module->functions()) {
         auto* function = program.getFunction(&func);
         for (const auto& block : func) {
             auto* myBlock = function->createBlockIfNotExist(&block);
-            auto* lastExpr = myBlock->expressions.back();
-            if (auto* gotoExpr = llvm::dyn_cast_or_null<GotoExpr>(lastExpr)) {
-                Block* target = gotoExpr->target;
-                if (target->doInline) {
-                    myBlock->expressions.erase(myBlock->expressions.end() - 1);
-                    std::copy(target->expressions.begin(), target->expressions.end(), std::back_inserter(myBlock->expressions));
-                }
+            InliningVisitor iv(myBlock);
+
+            for (auto& expr : myBlock->expressions) {
+                expr->accept(iv);
+                expr = iv.simplify(expr);
             }
         }
-
-        if (func.begin() == func.end())
-            continue;
-
     }
 
     program.addPass(PassType::InlineBlocks);
