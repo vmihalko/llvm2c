b68a133b39528da1137d4bbe0d1c5b19aea49eb4
Tomas Jasek <tomsik68@posteo.net>
2020-03-18 12:51:59 +0100

    tests: add tests for different types of main

---
+++ test/expected/main/main_int.c
@@ -0,0 +1,19 @@
+// function declarations
+int main(int var0, char** var1);
+extern unsigned int printf(unsigned char* var0, ...);
+
+// global variable definitions
+unsigned char _str[14] = {72,101,108,108,111,32,119,111,114,108,100,33,10,0,};
+
+int main(int var0, char** var1){
+    unsigned int var2;
+    unsigned int argc;
+    unsigned char** argv;
+    block0:
+    var2 = 0;
+    argc = var0;
+    argv = var1;
+    printf(&(_str[0]));
+    return 0;
+}
+
---
+++ test/expected/main/main_void.c
@@ -0,0 +1,13 @@
+// function declarations
+void main(void);
+extern unsigned int printf(unsigned char* var0, ...);
+
+// global variable definitions
+unsigned char _str[15] = {72,101,108,108,111,44,32,119,111,114,108,100,33,10,0,};
+
+void main(void){
+    block0:
+    printf(&(_str[0]));
+    return;
+}
+
