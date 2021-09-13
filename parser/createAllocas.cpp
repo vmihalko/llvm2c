b718c64b3493f9c197549f5e4bd23a940d05da4c
Marek Chalupa <mchqwerty@gmail.com>
2021-09-13 08:00:57 +0200

    Handle array allocas
    Create a call to alloca function in such a case.

--- parser/createAllocas.cpp
+++ parser/createAllocas.cpp
@@ -3,6 +3,8 @@
 #include "../core/Func.h"
 #include "../core/Block.h"
 
+#include "constval.h"
+
 void createAllocas(const llvm::Module* module, Program& program) {
     assert(program.isPassCompleted(PassType::CreateBlocks));
 
@@ -13,16 +15,50 @@ void createAllocas(const llvm::Module* module, Program& program) {
 
             for (const auto& ins : block) {
                 if (ins.getOpcode() == llvm::Instruction::Alloca) {
+                    std::unique_ptr<Value> theVariable;
+                    std::unique_ptr<StackAlloc> alloc;
+                    const auto *allocaInst = llvm::cast<const llvm::AllocaInst>(&ins);
+                    if (allocaInst->isArrayAllocation()) {
+                        llvm::errs() << *allocaInst << "\n";
+                        llvm::errs() << *allocaInst->getAllocatedType() << "\n";
+                        auto *llsize = allocaInst->getArraySize();
+                        Expr *size = nullptr;
+                        if (llvm::isa<llvm::ConstantInt>(llsize)) {
+                            size = createConstantValue(llsize, program);
+                        } else {
+                            size = program.getExpr(llsize);
+                        }
+                        if (!size) {
+                            llvm::errs() << "Unhandled size argument of alloca: " << *allocaInst << "\n";
+                            throw std::invalid_argument("Unhandled alloca");
+                        }
 
-                    const auto allocaInst = llvm::cast<const llvm::AllocaInst>(&ins);
+                       //theVariable = std::make_unique<Value>(func->getVarName(), func->getType(allocaInst->getType()));
+                       //alloc = std::make_unique<StackAlloc>(theVariable.get());
+                        std::vector<Expr*> params;
+                        params.push_back(size);
+                        auto allocacall = std::make_unique<CallExpr>(
+                                nullptr,
+                                "alloca",
+                                params,
+                                func->getType(allocaInst->getType()));
 
-                    auto theVariable = std::make_unique<Value>(func->getVarName(), func->getType(allocaInst->getAllocatedType()));
-                    auto alloc = std::make_unique<StackAlloc>(theVariable.get());
-
+                        // assign the result of the call to the variable
+                        theVariable = std::make_unique<Value>(func->getVarName(), allocacall->getType());
+                        alloc = std::make_unique<StackAlloc>(theVariable.get());
+                        auto assign = std::make_unique<AssignExpr>(theVariable.get(), allocacall.get());
+                        myBlock->addExprAndOwnership(std::move(alloc));
+                        myBlock->addOwnership(std::move(allocacall));
+                        myBlock->addExprAndOwnership(std::move(assign));
+                        //func->createExpr(&ins, std::move(allocacall));
+                    } else  {
+                        // normal alloca on the stack
+                        theVariable = std::make_unique<Value>(func->getVarName(), func->getType(allocaInst->getAllocatedType()));
+                        alloc = std::make_unique<StackAlloc>(theVariable.get());
+                        myBlock->addExprAndOwnership(std::move(alloc));
+                    }
                     func->createExpr(&ins, std::make_unique<RefExpr>(theVariable.get(), program.typeHandler.pointerTo(theVariable.get()->getType())));
-
                     myBlock->addOwnership(std::move(theVariable));
-                    myBlock->addExprAndOwnership(std::move(alloc));
                 }
             }
 
