2eb4c04069d0ead0988b0363994681ede99e04fb
Vincent Mihalkovic <vmihalko@redhat.com>
2022-10-19 21:45:58 +0200

    test: add types.c file

---
+++ test/types.c
@@ -0,0 +1,31 @@
+
+int foo(int[*]);
+
+
+struct lol {
+    int pica;
+} a;
+
+union kokot {
+    int llvm2c;
+}c ;
+
+
+int b[] = {0};
+
+int main(int argc, char**argv) {
+    if (argc == 2) {
+        int unsigned * b;
+        int* a = b + 1;
+        b = 8;
+    }
+
+    int (*f)(unsigned int);
+
+    int arr[4] = {1,2,3};
+    int a = arr[1];
+
+    foo(arr);
+
+    return arr[0];
+}
