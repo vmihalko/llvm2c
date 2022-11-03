209214e54f3116d045c4846e6d59b0c2c3dc474d
Vincent Mihalkovic <vmihalko@redhat.com>
2022-11-03 21:23:36 +0100

    test: fix the csmith testing script

--- test/csmith_testing.py
+++ test/csmith_testing.py
@@ -4,13 +4,14 @@ num_tests = 3
 csmith_runtime_dir = 'csmith-runtime'
 
 import tempfile, os, getopt, sys, shutil
-from subprocess import run, DEVNULL
+from subprocess import run, STDOUT, DEVNULL
 
 # TODO:
 # * hardcoded paths
 # * csmith arguments [simple, full_grammar]
 # * llvm2c coredump!
 
+llvm2c = "/home/vmihalko/DIPLO/llvm2c/build/llvm2c"
 def run_tests(how_many, arg_compile, arg_keep, dir_name):
     for i in range(how_many):
         with tempfile.NamedTemporaryFile(dir=dir_name) as tmp_file_name:
@@ -18,12 +19,16 @@ def run_tests(how_many, arg_compile, arg_keep, dir_name):
             tmp_file_c = tmp_file + '.c'
             tmp_file_re_c = tmp_file + '_re.c'
             tmp_file_ll = tmp_file + '.ll'
-            run(['csmith', '--output',  tmp_file_c])
+            rcsmith = run(['csmith', '--output',  tmp_file_c])
             run(['clang', '-S', '-emit-llvm', tmp_file_c, '-o', tmp_file_ll], stdout=DEVNULL, stderr=DEVNULL)
-            proc = run(['../build/llvm2c', '-o', tmp_file_re_c, tmp_file_ll])
+            #run([llvm2c, '-o', tmp_file_re_c, tmp_file_ll], stdout=STDOUT)
+            proc = run(['../build/llvm2c', '-o', tmp_file_re_c, tmp_file_ll], capture_output=True, text=True)
 
             if proc.returncode != 0:
-                print("llvm2c was not able to process inputed file!")
+                path, oflag, outpt, inpt = proc.args 
+                print(f"{path} {oflag} {outpt} {inpt}")
+                print("llvm2c was not able (coredumped?) to process inputed file!")
+                print(f"{proc.stderr}{proc.stdout}")
                 break
 
             if arg_compile:
