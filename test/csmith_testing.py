19efab1dd9894c31d64be47b4249d14df39ba0a3
Vincent Mihalkovic <vmihalko@redhat.com>
2022-11-03 21:23:36 +0100

    test: fix the csmith testing script

--- test/csmith_testing.py
+++ test/csmith_testing.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python3
 
-num_tests = 1000
+num_tests = 3
 csmith_runtime_dir = 'csmith-runtime'
 
-import tempfile, os
+import tempfile, os, getopt, sys, shutil
 from subprocess import run, DEVNULL
 
-dirname = 'csmith-tests'
-for i in range(num_tests):
-    run(['./csmith', '--output', dirname + '/program.c'])
-    run(['clang', '-I', csmith_runtime_dir, '-S', '-emit-llvm', dirname + '/program.c'])
-    proc = run(['./llvm2c', '-o', dirname + '/output.c', dirname + '/program.ll'])
+# TODO:
+# * hardcoded paths
+# * csmith arguments [simple, full_grammar]
+# * llvm2c coredump!
 
-    if proc.returncode != 0:
-        print("Found bug!")
-        break
+def run_tests(how_many, arg_compile, arg_keep, dir_name):
+    for i in range(how_many):
+        with tempfile.NamedTemporaryFile(dir=dir_name) as tmp_file_name:
+            tmp_file = tmp_file_name.name
+            tmp_file_c = tmp_file + '.c'
+            tmp_file_re_c = tmp_file + '_re.c'
+            tmp_file_ll = tmp_file + '.ll'
+            run(['csmith', '--output',  tmp_file_c])
+            run(['clang', '-S', '-emit-llvm', tmp_file_c, '-o', tmp_file_ll], stdout=DEVNULL, stderr=DEVNULL)
+            proc = run(['../build/llvm2c', '-o', tmp_file_re_c, tmp_file_ll])
 
-    proc = run(['clang', dirname + '/output.c'])
-    if proc.returncode != 0:
-        print("Found bug!")
-        break
+            if proc.returncode != 0:
+                print("llvm2c was not able to process inputed file!")
+                break
+
+            if arg_compile:
+                proc = run(['clang', tmp_file_re_c])
+                if proc.returncode != 0:
+                    print("llv2c outputed file is uncompilable!")
+                    break
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "cekh",["--compile", "--execute", "--keep", "--tests"])
+    except:
+        print("argument parsing error")
+
+    dirname = 'csmith-tests'
+    if os.path.exists(dirname):
+        shutil.rmtree(dirname)
+    os.mkdir(dirname)
+    
+    run_tests(1, False, False, dirname)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
