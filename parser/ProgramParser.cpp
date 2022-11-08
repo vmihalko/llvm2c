aaeedb2001fbe74cb29a05d643156fc186a70f2e
xmihalk <mihalkovic.vincent@gmail.com>
2022-11-08 12:08:52 +0100

    parser: add support for legacy pass manager
    Enable all (loop) passes from https://github.com/llvm-mirror/llvm/blob/master/lib/Transforms/IPO/PassManagerBuilder.cpp#L353-#L375:
    ```c
      if (EnableSimpleLoopUnswitch) {
        // The simple loop unswitch pass relies on separate cleanup passes. Schedule
        // them first so when we re-process a loop they run before other loop
        // passes.
        MPM.add(createLoopInstSimplifyPass());
        MPM.add(createLoopSimplifyCFGPass());
      }
      // Rotate Loop - disable header duplication at -Oz
      MPM.add(createLoopRotatePass(SizeLevel == 2 ? 0 : -1));
      MPM.add(createLICMPass(LicmMssaOptCap, LicmMssaNoAccForPromotionCap));
      if (EnableSimpleLoopUnswitch)
        MPM.add(createSimpleLoopUnswitchLegacyPass());
      else
        MPM.add(createLoopUnswitchPass(SizeLevel || OptLevel < 3, DivergentTarget));
      // FIXME: We break the loop pass pipeline here in order to do full
      // simplify-cfg. Eventually loop-simplifycfg should be enhanced to replace the
      // need for this.
      MPM.add(createCFGSimplificationPass());
      addInstructionCombiningPass(MPM);
      // We resume loop passes creating a second loop pipeline here.
      MPM.add(createIndVarSimplifyPass());        // Canonicalize indvars
      MPM.add(createLoopIdiomPass());             // Recognize idioms like memset.
    ```
    Test:
    ```bash
    clang -S -emit-llvm -Xclang -disable-O0-optnone simple-for-loop-second-latch.c -o simple-for-loop-second-latch-noopt.ll
    optpassPasses simple-for-loop-second-latch-noopt --loop-simplify --simplifycfg --loop-rotate --lcssa --licm --loop-unswitch --simplifycfg --instcombine --indvars
    old_llvm2c simple-for-loop-second-latch-noopt-opt == new_llvm2c simple-for-loop-second-latch-noopt
    ```

--- parser/ProgramParser.cpp
+++ parser/ProgramParser.cpp
@@ -5,11 +5,100 @@
 #include <llvm/IRReader/IRReader.h>
 #include <iostream>
 
+
+#include <llvm/Transforms/Utils/SimplifyCFGOptions.h>
+#include <functional>
+#include "llvm/Analysis/GlobalsModRef.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/DynamicLibrary.h"
+#include "llvm/Support/PluginLoader.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/IPO.h"
+#include "llvm/Transforms/IPO/FunctionAttrs.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm-c/Transforms/PassManagerBuilder.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Analysis/BasicAliasAnalysis.h"
+#include "llvm/Analysis/CFLAndersAliasAnalysis.h"
+#include "llvm/Analysis/CFLSteensAliasAnalysis.h"
+#include "llvm/Analysis/GlobalsModRef.h"
+#include "llvm/Analysis/InlineCost.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/Analysis/ScopedNoAliasAA.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Analysis/TypeBasedAliasAnalysis.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Transforms/AggressiveInstCombine/AggressiveInstCombine.h"
+#include "llvm/Transforms/IPO.h"
+#include "llvm/Transforms/IPO/Attributor.h"
+#include "llvm/Transforms/IPO/ForceFunctionAttrs.h"
+#include "llvm/Transforms/IPO/FunctionAttrs.h"
+#include "llvm/Transforms/IPO/InferFunctionAttrs.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Instrumentation.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Scalar/InstSimplifyPass.h"
+#include "llvm/Transforms/Scalar/LICM.h"
+#include "llvm/Transforms/Scalar/LoopUnrollPass.h"
+#include "llvm/Transforms/Scalar/SimpleLoopUnswitch.h"
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/Vectorize.h"
+#include "llvm/Transforms/Vectorize/LoopVectorize.h"
+#include "llvm/Transforms/Vectorize/SLPVectorizer.h"
+
+
 #define RUN_PASS(pass) \
     do {\
         pass(mod, result); \
     } while (0);
 
+// inspired by https://github.com/klee/klee/blob/v2.3/lib/Module/Optimize.cpp#L83-L92
+static inline void addPass(llvm::legacy::PassManager &PM, llvm::Pass *P) {
+  // Add the pass to the pass manager...
+  PM.add(P);
+
+  PM.add(llvm::createVerifierPass());
+}
+
+void run_llvm_passes(llvm::Module &mod) {
+    llvm::legacy::PassManager passes;
+    
+    addPass(passes, llvm::createLoopInstSimplifyPass());
+    addPass(passes, llvm::createLoopSimplifyCFGPass());
+
+
+    addPass(passes, llvm::createLoopRotatePass());
+    addPass(passes, llvm::createLICMPass());
+    addPass(passes, llvm::createSimpleLoopUnswitchLegacyPass());
+    // addPass(passes,llvm::createLoopUnswitchPass());
+    addPass(passes,llvm::createCFGSimplificationPass());
+    addPass(passes,llvm::createIndVarSimplifyPass());
+    addPass(passes,llvm::createLoopIdiomPass());
+
+    // addPass(passes,llvm::createLoopInterchangePass());
+    // addPass(passes,);
+    // addPass(passes,);
+    // addPass(passes,);
+    // addPass(passes,);
+    passes.run(mod);
+
+    // llvm::createVerifierPass();
+
+}
 
 Program ProgramParser::parse(const std::string& file, bool bitcastUnions) {
     Program result;
@@ -21,6 +110,7 @@ Program ProgramParser::parse(const std::string& file, bool bitcastUnions) {
         throw std::invalid_argument("Error loading module - invalid input file:\n" + file + "\n");
     }
 
+    //run_llvm_passes(*module);
     const auto* mod = module.get();
 
     RUN_PASS(determineIncludes);
