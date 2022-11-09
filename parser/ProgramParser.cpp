6e4bcfb14c5397589ca1c960b30283c840290214
Vincent Mihalkovic <vmihalko@redhat.com>
2022-11-09 17:05:00 +0100

    parser: parse the llvm::LoopInfo datastructure

--- parser/ProgramParser.cpp
+++ parser/ProgramParser.cpp
@@ -110,7 +110,7 @@ Program ProgramParser::parse(const std::string& file, bool bitcastUnions) {
         throw std::invalid_argument("Error loading module - invalid input file:\n" + file + "\n");
     }
 
-    //run_llvm_passes(*module);
+    run_llvm_passes(*module);
     const auto* mod = module.get();
 
     RUN_PASS(determineIncludes);
